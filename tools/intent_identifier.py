714254fb427524865e9fa9595b3e83f5813a3a14
space8412 <summit8412@gmail.com>
2025-05-01 12:19:48 +0900

    Delete tools directory

--- tools/intent_identifier.py
+++
@@ -1,8 +0,0 @@
-def identify_intent(text):
-    text = text.lower()
-    for intent, keywords in {
-        'create': ['등록', '추가', '만들어', '잡아', '넣어', '생성'],
-    }.items():
-        if any(k in text for k in keywords):
-            return intent
-    return 'create'
